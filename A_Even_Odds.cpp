25fcabda85666c6b5a3d461511b5bff5c2ac991d
piyushsonawane07 <piyushsonawane140@gmail.com>
2023-01-04 08:10:32 +0530

    04-01-2022

---
+++ A_Even_Odds.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    long long n, k; cin >> n >> k;
+
+    if(k <= (n + 1) / 2)
+        cout << k * 2 - 1;
+    else
+        cout << (k - (n + 1) / 2) * 2;
+    
+    return 0;
+}
